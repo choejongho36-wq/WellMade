d25aa73fd0ed8078be5b39650f20684129e03c0e
Hyeongjun79 <kwenhj@gmail.com>
2026-08-13 16:39:36 +0900

    feat:backend start

---
+++ backend/src/test/java/com/kdt/wellmade/WellmadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.wellmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellmadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
